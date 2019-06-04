c5b3b4a81c86b215924e659b2e791568b0208a11
diegoranmtz <50603289+diegoranmtz@users.noreply.github.com>
2019-06-03 20:35:18 -0500

    Update Perro.cs

--- Actividad1/Actividad1/Perro.cs
+++ Actividad1/Actividad1/Perro.cs
@@ -3,7 +3,7 @@
     class Perro
     {
         public string nombre { get; set; }
-        private int edad { get; set; }
+        private int años { get; set; }
         public string raza { get; set; }
         public Suscursal suscursal { get; set; }
     }
